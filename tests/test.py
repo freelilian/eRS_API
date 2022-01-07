e76b1fbfaf70ad8a088e753f452a519d0e54a3cf
Lijie Guo <lijieg@yoga.localdomain>
2022-01-06 21:18:47 -0500

    update alnative endpoints

--- tests/test.py
+++ tests/test.py
@@ -10,5 +10,7 @@ with open('test_ratings.json') as f:
 host = 'http://127.0.0.1:5000'
 
 endpoint = 'preferences'
+# endpoint = 'emotions'
+# endpoint = 'latent_features'
 res = requests.post(f' {host}/{endpoint}', json=dict(ratings=data['ratings'], user_id=None))
 print(res.text)
